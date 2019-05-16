2c8a2f73d1a9c5bafe1354f3a122bd50549c385a
zhivkoststyoanov <zhivkostoyanovbg@gmail.com>
2019-05-16 14:58:38 +0300

    New Files

---
+++ 9.Interfaces-and-Abstraction/Interfaces-and-Abstraction/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Student
+ *
+ * @author User
+ */
+ 
+ class Student extends Person{
+     
+        public function printData(){
+         echo $this->name;
+     }
+ }
